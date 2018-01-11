e8496eaee9ba95b3d9aeee9d6e68be67c7e10480
Sagar Popat <sagar.popat@flipkart.com>
2018-01-12 01:18:04 +0530

    Bug fix

--- utils/config.py
+++ utils/config.py
@@ -1,17 +1,23 @@
 import ConfigParser
 from utils.config import *
+import os
 
 def get_allvalues(section):
+	# Return all the values in dict format from config file
 	Config = ConfigParser.ConfigParser()
 	Config.read('utils/config.property')
 	return dict(Config.items(section))
 
-def get_value(section,name):
+def get_value(filename,section,name):
+	# Return only one value from config file
+	print os.getcwd()
+	file_name = 'utils/'+filename
 	Config = ConfigParser.ConfigParser()
-	Config.read('utils/config.property')
+	Config.read(file_name)
 	return Config.get(section,name)
 
 def update_value(section,name,value):
+	# This function deals with updating value in config file
 	config= ConfigParser.RawConfigParser()
 	config.read('utils/config.property')
 	config.set(section,name,value)
