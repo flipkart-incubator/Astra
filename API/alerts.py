ac0d2996c03a1f90bd51d008c355fe9140df37c7
Sagar Popat <sagar.popat@flipkart.com>
2018-01-15 01:33:24 +0530

    Added Report feature

--- API/alerts.py
+++ API/alerts.py
@@ -1,14 +1,16 @@
 import ast
 import json
 import sys
+import os
 
 sys.path.append('../')
 
 from pymongo import MongoClient
-from utils.vulnerabilities import test
-from flask import Flask, jsonify
+from utils.vulnerabilities import vuln
+from flask import Flask, jsonify,render_template
 
-app = Flask(__name__)
+
+app = Flask(__name__,template_folder='template')
 
 def db_connect():
 		client = MongoClient('localhost',27017)
@@ -16,14 +18,23 @@ def db_connect():
 		db = client.apiscan
 
 def fetch_records():
+		print "sss"
 		db_connect()
 		vul_list = []
 		records = db.vulnerbilities.find({})
+		print records
 		if records:
 			for data in records:
 				data.pop('_id')
 				data =  ast.literal_eval(json.dumps(data))
 				try:
+					print "iiddd",	data['id']
+					if data['id'] == "NA":
+							print "ins"
+							all_data = {'url' : data['url'], 'impact' : data['impact'], 'name' : data['name'], 'req_headers' : data['req_headers'], 'req_body' : data['req_body'], 'res_headers' : data['res_headers'], 'res_body' : data['res_body'], 'Description' : data['Description'], 'remediation' : data['remediation']}
+							print "aaa",all_data
+							vul_list.append(all_data)
+
 					if data['id']:
 						for vul in test:
 							if data['id'] == vul['id']:
@@ -38,7 +49,6 @@ def fetch_records():
 							    			'Description' : vul['Description'],
 							    			'remediation' : vul['remediation']
 							    			}
-	
 							    vul_list.append(all_data)
 							    break
 
@@ -47,9 +57,15 @@ def fetch_records():
 
 			return vul_list
 
+@app.route('/report', methods=['GET'])
+def alerts_report():
+	print "CCC",os.getcwd()
+	return render_template('report.html')
+
+
 @app.route('/alerts/', methods=['GET'])
 def return_alerts():
 	result = fetch_records()
 	return jsonify(result)
 
-app.run(debug=True,port=3234)
+app.run(debug=True,port=3229)
