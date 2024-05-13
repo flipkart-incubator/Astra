0e73140c992dbb3d2fa501e7ff5d0044ddebfb13
Surya <suryasidhartha.s@gmail.com>
2024-05-13 16:22:11 +0530

    fixed DB issues

--- utils/db.py
+++ utils/db.py
@@ -28,18 +28,18 @@ class Database_update:
 
 	def insert_record(self,data):
 		try:
-			self.db.vulnerabilities.insert(data)
+			self.db.vulnerabilities.insert_one(data)
 		except Exception as e:
 			raise e
 
 	def update_record(self,find,update):
 		try:
-			self.db.vulnerabilities.update(find,update)
+			self.db.vulnerabilities.update_one(find,update)
 		except Exception as e:
 			raise e
 
 	def update_scan_record(self,find,update):
 		try:
-			self.db.scanids.update(find,update)
+			self.db.scanids.update_one(find,update)
 		except Exception as e:
 			raise e
